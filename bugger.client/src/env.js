269e00304b0bfb6436f3c19b80a4077d5a096f84
Melia88 <castro.melissa8.com>
2021-04-30 13:39:13 -0600

    intit commit

---
+++ bugger.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'melia88.us.auth0.com'
+export const audience = 'https://todo-api.com'
+export const clientId = 'D7UVniD8W6ZCk4tmSCNfvRDBq6fwncLX'
